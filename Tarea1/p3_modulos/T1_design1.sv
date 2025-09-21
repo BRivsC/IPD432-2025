fa31cf34bae0fa5d8bb9fad0d53c4c96c7c8486c
BRivsC <bastian.rivas@sansano.usm.cl>
2025-09-21 16:07:04 -0300

    Implementado módulo top + testbench

--- Tarea1/p3_modulos/T1_design1.sv
+++ Tarea1/p3_modulos/T1_design1.sv
@@ -10,4 +10,30 @@ module T1_design1 #(
     output  logic   IncPulse_out
 );
 
+logic rst;
+assign rst = ~resetN;
+
+PB_Debouncer_FSM #(
+    .DELAY                (N_DEBOUNCER_DELAY)   // Number of clock pulses to check stable button pressing
+) PB_Debouncer (
+    .clk                  (clk), // base clock
+	.rst                  (rst), // global reset
+	.PB                   (PushButton),  // raw asynchronous input from mechanical PB         
+	
+    .PB_pressed_status    (PB_status),  // clean and synchronized pulse for button pressed
+	.PB_pressed_pulse     (),   // high if button is pressed
+    .PB_released_pulse    ()    // clean and synchronized pulse for button released
+);
+
+
+PB_FSM #(
+    .N_INCREMENT_DELAY_CONTINUOUS    (N_INCREMENT_DELAY_CONTINUOUS)
+) u_PB_FSM (
+    .clk                             (clk),
+    .resetN                          (resetN),
+    .PB_status                       (PB_status),
+    .IncPulse_out                    (IncPulse_out)
+);
+
+
 endmodule
---
+++ Tarea1/p3_modulos/T1_design1_testbench.sv
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+// Testbench para el módulo top de la pregunta 3 de la tarea 1
+module T1_design1_testbench();
+
+logic clk, resetN, PushButton, IncPulse_out;
+
+T1_design1 #(
+    .N_DEBOUNCER_DELAY               (10),
+    .N_INCREMENT_DELAY_CONTINUOUS    (5)
+) u_T1_design1 (
+    .clk                             (clk),
+    .resetN                          (resetN),
+    .PushButton                      (PushButton),
+    .IncPulse_out                    (IncPulse_out)
+);
+
+always #1 clk = ~clk;
+
+initial begin
+    clk = 1'b0;
+    resetN = 1'b0;
+    PushButton = 1'b0;
+    #5 resetN = 1'b1;
+    #10
+    // Pulsación larga 1
+    PushButton = 1'b1;
+    #20 PushButton = 1'b0;
+
+    // Pulsación corta 1
+    PushButton = 1'b1;
+    #3 PushButton = 1'b0;
+    #17
+
+    // Pulsación corta 2
+    PushButton = 1'b1;
+    #6 PushButton = 1'b0;
+    #24
+
+    // Pulsación del largo del delay
+    PushButton = 1'b1;
+    #10 PushButton = 1'b0;
+    #20
+
+    // Pulsación más larga 
+    PushButton = 1'b1;
+    #100 PushButton = 1'b0;
+
+    // Fin de la simulación
+    #50
+    $finish;
+end
+
+endmodule
