75de4413f7bd11dddf6fc0f13cbed3ebca2834b2
BRivsC <bastian.rivas@sansano.usm.cl>
2025-10-22 19:42:16 -0300

    Actualizados IO de inputInterface

--- Tarea 2/Sources/inputInterface.sv
+++ Tarea 2/Sources/inputInterface.sv
@@ -4,11 +4,12 @@
 module inputInterface#(
     parameter NUM_ELEMENTOS = 1024
 )(
-    input logic clk, reset, rx_ready, 
+    input logic input_domain_clk, reset, rx_ready, processor_domain_clk, begin_write,
     input logic [7:0] rx_data,
-    input logic [9:0] bram_a_read_addr, bram_b_read_addr,
+    input logic [9:0] read_mem_dir,
+    output logic write_done, command_ready,
     output logic [7:0] command, //  Sigue formato para MainCtrl (dir memoria 0A, 1B, write, read, sum, avg, euc dist, man dist y dot prod)
-    output logic [9:0] bram_a_dout, bram_b_dout
+    output logic [9:0] data_a, data_b
     );
 
     logic [9:0] write_data;
@@ -23,7 +24,7 @@ module inputInterface#(
 
 
     writeCtrl u_writeCtrl (
-        .clk           (clk),
+        .clk           (input_domain_clk),
         .reset         (reset),
         .rx_ready      (rx_ready),
         .en            (en_write),   
@@ -41,7 +42,7 @@ module inputInterface#(
         .N        (10),
         .MAX_COUNT(NUM_ELEMENTOS)
     ) write_address_counter (
-        .clk      (clk),
+        .clk      (input_domain_clk),
         .reset    (reset),
         .inc      (inc),
         .count    (write_address),
@@ -55,7 +56,7 @@ module inputInterface#(
     assign opcode_in = rx_data[2:0];
     assign bram_info_in = rx_data[7];
     commandDecoder u_commandDecoder (
-        .clk            (clk),
+        .clk            (input_domain_clk),
         .reset          (reset),
         .rx_ready       (rx_ready),
         .op_code_in     (opcode_in),
@@ -66,33 +67,33 @@ module inputInterface#(
     );
 
     blk_mem_gen_0 BRAMA (
-        .clka(clk),                // input wire clka
+        .clka(input_domain_clk),   // input wire clka
         .ena(1),                   // input wire ena
         .wea(wea_a),               // input wire [0 : 0] wea
         .addra(write_address),     // input wire [9 : 0] addra
         .dina(write_data),         // input wire [9 : 0] dina
         .douta(),                  // output wire [9 : 0] douta
-        .clkb(clk),                // input wire clkb
+        .clkb(processor_domain_clk),                // input wire clkb
         .enb(1),                   // input wire enb
         .web(0),                   // input wire [0 : 0] web
-        .addrb(bram_a_read_addr),  // input wire [9 : 0] addrb
+        .addrb(read_mem_dir),  // input wire [9 : 0] addrb
         .dinb(10'd0),              // input wire [9 : 0] dinb
-        .doutb(bram_a_dout)        // output wire [9 : 0] doutb
+        .doutb(data_a)        // output wire [9 : 0] doutb
     );
 
     blk_mem_gen_0 BRAMB (
-        .clka(clk),                 // input wire clka
+        .clka(input_domain_clk),    // input wire clka
         .ena(1),                    // input wire ena
         .wea(wea_b),                // input wire [0 : 0] wea
         .addra(write_address),      // input wire [9 : 0] addra
         .dina(write_data),          // input wire [9 : 0] dina
         .douta(),                   // output wire [9 : 0] douta
-        .clkb(clk),                 // input wire clkb
+        .clkb(processor_domain_clk),// input wire clkb
         .enb(1),                    // input wire enb
         .web(0),                    // input wire [0 : 0] web
-        .addrb(bram_b_read_addr),   // input wire [9 : 0] addrb
+        .addrb(read_mem_dir),   // input wire [9 : 0] addrb
         .dinb(10'd0),               // input wire [9 : 0] dinb
-        .doutb(bram_b_dout)         // output wire [9 : 0] doutb
+        .doutb(data_b)         // output wire [9 : 0] doutb
     );
 
 
