3ab36249436f5890c4992dca23870673c00ddb67
SvenKleinP <75145103+SvenKleinP@users.noreply.github.com>
2025-10-23 21:48:21 -0300

    Top Module input y procesos
    Top module con inputs y procesos terminado. Falta conectar las señales tx_sent_src a la salida y op_done_dest a la salida.

--- Tarea 2/Sources/top_module.sv
+++ Tarea 2/Sources/top_module.sv
@@ -46,15 +46,15 @@ module top_module #(parameter NUM_ELEMENTOS = 1024)(
     logic send_b0, send_b1, send_b2, send_b3;//banderas de envio de bytes de resultado
     logic tx_done;//byte que indica que se envio un dato completo
     logic bcd_data;//datos bcd para el display
-    logic begin_write;
-    logic op_done;
+    logic begin_write_src, begin_write_dest;
+    logic op_done_src, op_done_dest;
     logic [9:0] read_mem_dir;
-    logic write_done;
-    logic command_ready;
+    logic write_done_src, write_done_dest;
+    logic command_ready_src, command_ready_dest;
     logic [7:0] command;
     logic [9:0] bram_a_dout;
     logic [9:0] bram_b_dout;
-    logic tx_sent;
+    logic tx_sent_src, tx_sent_dest;
     logic euc_op_done;
     logic process_control;
     logic read_enable;
@@ -85,6 +85,78 @@ module top_module #(parameter NUM_ELEMENTOS = 1024)(
         .clk_in1(clk)      // input clk_in1
     );
     
+    xpm_cdc_single/* #(
+    .DEST_SYNC_FF          (PL_DEST_SYNC_FF),
+    .REG_OUTPUT            (PL_REG_OUTPUT),
+    .RST_USED              (PL_RST_USED),
+    .SIM_ASSERT_CHK        (SIM_ASSERT_CHK)
+  )*/ 
+    single_begin_write (
+        .src_clk               (clk_process),
+        .src_in                (begin_write_src),
+        .dest_clk              (clk_input),
+        .dest_out              (begin_write_dest)
+    );
+    
+    xpm_cdc_pulse/* #(
+    .DEST_SYNC_FF          (PL_DEST_SYNC_FF),
+    .REG_OUTPUT            (PL_REG_OUTPUT),
+    .RST_USED              (PL_RST_USED),
+    .SIM_ASSERT_CHK        (SIM_ASSERT_CHK)
+  )*/ 
+    pulse_write_done (
+        .src_clk               (clk_input),
+        .src_pulse             (write_done_src),
+        .dest_clk              (clk_process),
+        .src_rst               (reset_input),
+        .dest_rst              (reset_process),
+        .dest_pulse            (write_done_dest)
+    );
+    
+    //command ready tambien
+    
+    xpm_cdc_pulse/* #(
+    .DEST_SYNC_FF          (PL_DEST_SYNC_FF),
+    .REG_OUTPUT            (PL_REG_OUTPUT),
+    .RST_USED              (PL_RST_USED),
+    .SIM_ASSERT_CHK        (SIM_ASSERT_CHK)
+  )*/ 
+    pulse_tx_sent (
+        .src_clk               (clk_output),
+        .src_pulse             (tx_sent_src),
+        .dest_clk              (clk_process),
+        .src_rst               (reset_input),
+        .dest_rst              (reset_process),
+        .dest_pulse            (tx_sent_dest)
+    );
+    
+    xpm_cdc_pulse/* #(
+    .DEST_SYNC_FF          (PL_DEST_SYNC_FF),
+    .REG_OUTPUT            (PL_REG_OUTPUT),
+    .RST_USED              (PL_RST_USED),
+    .SIM_ASSERT_CHK        (SIM_ASSERT_CHK)
+  )*/ 
+    pulse_begin_transmision (
+        .src_clk               (clk_process),
+        .src_pulse             (op_done_src),
+        .dest_clk              (clk_output),
+        .src_rst               (reset_process),
+        .dest_rst              (reset_input),
+        .dest_pulse            (op_done_dest)
+    );
+    
+    xpm_cdc_single /*#(
+    .DEST_SYNC_FF          (S_DEST_SYNC_FF),
+    .SIM_ASSERT_CHK        (SIM_ASSERT_CHK),
+    .SRC_INPUT_REG         (S_SRC_INPUT_REG)
+    )*/
+    single_command (
+        .src_clk               (clk_input),
+        .src_in                (command_ready_src),
+        .dest_clk              (clk_process),
+        .dest_out              (command_ready_dest)
+    );
+    
     uart_basic #(
 		.CLK_FREQUENCY(100_000_000), // reloj base de entrada
 		.BAUD_RATE(115200)
@@ -107,12 +179,12 @@ module top_module #(parameter NUM_ELEMENTOS = 1024)(
         .processor_domain_clk    (clk_process),
         .reset                   (reset_input),
         .rx_ready                (rx_ready),
-        .begin_write             (begin_write),
-        .op_done                 (op_done),
+        .begin_write             (begin_write_dest),
+        .op_done                 (op_done_dest),
         .rx_data                 (rx_data),
         .read_mem_dir            (read_mem_dir),
-        .write_done              (write_done),
-        .command_ready           (command_ready),
+        .write_done              (write_done_src),
+        .command_ready           (command_ready_src),
         .command                 (command),
         //  Sigue formato para MainCtrl (dir memoria 0A, 1B, write, read, sum, avg, euc dist, man dist y dot prod)
         .data_a                  (bram_a_dout),
@@ -125,15 +197,15 @@ module top_module #(parameter NUM_ELEMENTOS = 1024)(
     ) u_controllUnit (
         .clk                  (clk_process),                //reloj 100Mhz
         .reset                (reset_process),              //reset sincronizado
-        .command_ready        (command_ready),      //señal para cambiar de comando de IDLE a una operacion
-        .write_done           (write_done),         //señal que indica que se termino toda la operacion de escritura
-        .tx_sent              (tx_sent),            //señal de que se envio un dato completo
+        .command_ready        (command_ready_dest),      //señal para cambiar de comando de IDLE a una operacion
+        .write_done           (write_done_dest),         //señal que indica que se termino toda la operacion de escritura
+        .tx_sent              (tx_sent_dest),            //señal de que se envio un dato completo
         .op_ready             (euc_op_done),           //señal de que la operacion euc dist esta lista
         .command              (command),            //dir memoria 0A, 1B, dot prod, man dist, euc dist, avg, sum, read y write. En ese orden
         .process_ctrl         (process_control),       //seña para varios controles del processing core
         .read_enable          (read_enable),        //enable para las memorias para leer
-        .begin_transmision    (op_done),  //señal para iniciar la transmision cuando hay un resultado listo
-        .begin_write          (begin_write),        //señal para empezar la escritura
+        .begin_transmision    (op_done_src),  //señal para iniciar la transmision cuando hay un resultado listo
+        .begin_write          (begin_write_src),        //señal para empezar la escritura
         .enables              (enables),            //arreglo de enables para las distintas operaciones. Mismo orden que command
         //direccion de memoria a leer
         //.mem_dir              (read_mem_dir)
